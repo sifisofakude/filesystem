41ed1ae8e8c2a6f71fd18ea029cd51f82cce37fd
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-09 05:53:10 +0200

    test: Added filesystem test suite [skip jvm]

--- src/iosTest/kotlin/io/github/sifisofakude/filesystem/IosFileSystemTest.kt
+++ src/iosTest/kotlin/io/github/sifisofakude/filesystem/IosFileSystemTest.kt
@@ -365,7 +365,7 @@ class IosFileSystemTest {
                 destination
             )
             
-            println("copy results: $results")
+            println("copy results: $result")
 // 
 //             assertEquals(
 //                 destination,
