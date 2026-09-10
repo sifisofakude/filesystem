17931b61819f7999fd909fc2a3535a74971280c9
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-10 05:07:51 +0200

    test: Added android test suite [skip apple] [skip jvm]

---
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/SafPickerActivity.kt
@@ -0,0 +1,38 @@
+package io.github.sifisofakude.filesystem
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SafPickerActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+            addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION)
+        }
+
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE) {
+            setResult(resultCode, data)
+            finish()
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 100
+    }
+}
