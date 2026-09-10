7f354ae088796ff3438ccb06e3dd6c3be4c0ea6a
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-10 07:54:48 +0200

    test: Added android test suite [skip apple] [skip jvm]

--- src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
@@ -3,10 +3,15 @@ package io.github.sifisofakude.filesystem
 import android.content.Intent
 import android.net.Uri
 import android.content.Context
+import android.app.Activity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
+
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
