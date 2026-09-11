c4bc39c8a4febc19b1c6abcfc9a698ca45f3b005
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-11 05:51:05 +0200

    test: Added android test suite [skip apple] [skip jvm]

--- src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
@@ -199,35 +199,36 @@ class AndroidSafFileSystemTest {
     		val root = fs.getParentFile(uri) ?: return null
 
     		// selectFile(root,name)
+    		null
     	}else	{
     		selectFolder(uri)
     	}
     }
 
-    private fun selectFile(root: String, name: String): Uri?	{
-    	SafPickerActivity.resultUri = null
-    	SafPickerActivity.resultCode = Activity.RESULT_CANCELED
-    	
-    	val intent = Intent(context,SafPickerActivity::class.java).apply	{
-    		addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    		putExtra("selectUri",root)
-    	}
-
-    	instrumentation.startActivitySync(intent)
-
-      device.wait(
-      	Until.findObject(
-      		By.clazzName("android.widget.TextView").text(name)
-      	),
-      	5000
-      )?.click()
-
-    	return try	{
-    		waitForPickerResult()
-    	}catch(_: IllegalArgumentException)	{
-    		null
-    	}
-    }
+//     private fun selectFile(root: String, name: String): Uri?	{
+//     	SafPickerActivity.resultUri = null
+//     	SafPickerActivity.resultCode = Activity.RESULT_CANCELED
+//     	
+//     	val intent = Intent(context,SafPickerActivity::class.java).apply	{
+//     		addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//     		putExtra("selectUri",root)
+//     	}
+// 
+//     	instrumentation.startActivitySync(intent)
+// 
+//       device.wait(
+//       	Until.findObject(
+//       		By.clazzName("android.widget.TextView").text(name)
+//       	),
+//       	5000
+//       )?.click()
+// 
+//     	return try	{
+//     		waitForPickerResult()
+//     	}catch(_: IllegalArgumentException)	{
+//     		null
+//     	}
+//     }
 
     private fun selectFolder(uri: String): Uri?	{
     	SafPickerActivity.resultUri = null
