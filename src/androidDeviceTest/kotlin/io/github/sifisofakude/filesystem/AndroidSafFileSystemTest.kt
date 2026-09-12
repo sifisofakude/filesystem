4f529b37e406019d9d77d27d20c84eed24394a86
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-12 12:33:45 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
@@ -65,7 +65,6 @@ class AndroidSafFileSystemTest {
 
         fail("Created file: ${fs.getDocumentFile(root.toString())?.uri}")
     
-    //     assertNotNull(fs.createDirectory("docs"))
     //     assertTrue(fs.exists("docs"))
     //     assertTrue(fs.isDirectory("docs"))
     // 
