fa99378026c179b9347728e6f24b85e0aef03506
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-12 06:29:19 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -220,7 +220,9 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 		while(true)	{
 			if(relativeUri.isNotEmpty())	{
 				DocumentFile.fromTreeUri(context,Uri.parse(relativeUri))?.let	{
-					throw IllegalStateException("Relative path from: $relativeUri ${relativeNames.asReversed().joinToString("/")}")
+					if(it.exists())	{
+						throw IllegalStateException("Relative path from: $relativeUri ${relativeNames.asReversed().joinToString("/")}")
+					}
 				}
 			}
 			
