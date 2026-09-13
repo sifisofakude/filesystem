813044877d6405d790ab6b593145ebb6ac8a0074
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-13 18:41:43 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
@@ -60,7 +60,7 @@ class AndroidSafFileSystemTest {
         val root = selectFolder(constructUri("Root1/jane/doe"))
             ?: error("Could not select Root1")
     
-       	fail(root.toString(s))
+       	fail(root.toString())
     }
 
     @Test
