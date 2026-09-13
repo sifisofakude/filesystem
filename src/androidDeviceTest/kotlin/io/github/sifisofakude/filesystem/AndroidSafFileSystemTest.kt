7ca5dc13a86118dc8bd751d3cd872d6754e5b030
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-13 15:15:38 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
@@ -63,7 +63,7 @@ class AndroidSafFileSystemTest {
         fs.changeSelectedDirectory(root)
         // val file = "bane/cane/test.txt"
 
-        fail("Created file: ${fs.createDirectory("bane/mame")}")
+        fail("Created file: ${root}")
     
     //     assertTrue(fs.isDirectory("docs"))
     // 
