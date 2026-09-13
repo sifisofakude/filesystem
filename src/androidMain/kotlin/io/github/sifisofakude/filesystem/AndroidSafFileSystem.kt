4695ce877b479aa46564350407d6c271fc772660
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-13 15:10:43 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -500,7 +500,7 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 	 * @return URI string of the final directory, or null if creation failed
 	 */
 	override fun createDirectory(path: String): String? {
-			throw IllegalStateException("File not found: selectedParentUri")
+			throw IllegalStateException("File not found: $selectedParentUri")
 		if(isSafContext(path))	{
 			val relativeUri = if(isSafUri(path))	{
 				relativePathFromUri(path)
