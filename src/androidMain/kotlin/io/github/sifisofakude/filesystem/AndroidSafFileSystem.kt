49d265a49b924953971123296006634fac95cd42
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-13 18:14:18 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -98,7 +98,7 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 	fun changeSelectedDirectory(newParentUri: Uri?)	{
 		newParentUri?.let	{ parent ->
 			getDocumentFile(parent.toString())?.let	{ df ->
-				if(df.isDirectory )	{
+				if(df.exists() && df.isDirectory )	{
 					throw IllegalStateException("can't do this")
 					selectedParentUri = newParentUri
 				}
