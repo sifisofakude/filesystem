8ee150444faa9131c62966d69d6ab34471fdc2d4
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-14 09:40:02 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -519,6 +519,7 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 				relativePathFromUri(combinePath(selectedParentUri.toString(),path))
 			}
 			throw IllegalStateException("cant dobyhis")
+			
 			if(relativeUri.relativePath.isEmpty()) return null
 
 			var currentUri: String? = relativeUri.rootUri
