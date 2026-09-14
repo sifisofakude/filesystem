4e07a2632a2afb19f666bbb2a6e052bb0c78f377
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-14 06:40:57 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -97,7 +97,7 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 	 */
 	fun changeSelectedDirectory(newParentUri: Uri?)	{
 		newParentUri?.let	{ parent ->
-			if(isTreeUri(parent,toString()))	{
+			if(isTreeUri(parent.toString()))	{
 				DocumentFile.fromTreeUri(context,parent)?.let	{
 					if(it.isDirectory) selectedParentUri = newParentUri
 				}
