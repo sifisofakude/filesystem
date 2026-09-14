ec124ffb3e6858eda0e601c733ff7f1cc35719a7
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-14 09:15:13 +0200

    test: Added android test suite [skip jvm] [skip apple]

--- src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
+++ src/androidDeviceTest/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystemTest.kt
@@ -73,7 +73,7 @@ class AndroidSafFileSystemTest {
         assertNotNull(fs.resolveRelativeUri(root,"got/to/go/za"))
         assertNotNull(fs.getDocumentFile("jane/doe"))
     
-       	fail("${fs.crreateDirectory("got/to/go/za")}")
+       	fail("${fs.createDirectory("got/to/go/za")}")
     }
 
     @Test
