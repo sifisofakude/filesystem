cb07a11a11ea33509c353a421b5790a77a286d88
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-19 11:32:56 +0200

    test: Added android test suite [skip jvm] [skip ios]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -612,7 +612,7 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 
     		return getDocumentFile(parent)?.let	{
     			var success = false
-    		throw IllegalStateException("Created file: ${parent.uri.toString()}")
+    		throw IllegalStateException("Created file: ${it.uri.toString()}")
     			it.findFile(fileName)?.let { file ->
     				if(file.isFile) success = true
     			}
