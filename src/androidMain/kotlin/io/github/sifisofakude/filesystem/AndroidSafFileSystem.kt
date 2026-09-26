b480609f863d2b480922e234c7afb67c7f8e014c
Sifiso Fakude <sifisofakude404@gmail.com>
2026-09-26 17:03:03 +0200

    test: Added android test suite [skip jvm] [skip ios]

--- src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
+++ src/androidMain/kotlin/io/github/sifisofakude/filesystem/AndroidSafFileSystem.kt
@@ -758,7 +758,7 @@ class AndroidSafFileSystem(context: Context) : JvmFileSystem()	{
 
 			return try	{
 				contentResolver.query(
-					resolvedUri,
+					Uri.parse(resolvedUri),
 					arrayOf(DocumentsContract.Document.COLUMN_DOCUMENT_ID),
 					null,null,null
 				)?.use { cursor ->
